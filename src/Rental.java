7816c2804b84ff46dbdb560bb33fa224cbe73005
unknown <till.a@freenet.de>
2015-04-21 11:50:34 +0200

    7. getCharge() und getFrequentRenterPoints() nach movie verschoben

--- src/Rental.java
+++ src/Rental.java
@@ -1,40 +1,22 @@
 class Rental {
-    private Movie movie;
-    private int daysRented;
+    private Movie _movie;
+    private int _daysRented;
     public Rental(Movie newmovie, int newdaysRented) {
-        movie = newmovie;
-        daysRented = newdaysRented;
+        _movie = newmovie;
+        _daysRented = newdaysRented;
     }
     public int getDaysRented() {
-        return daysRented;
+        return _daysRented;
     }
     public Movie getMovie() {
-        return movie;
+        return _movie;
     }
     
-	double getCharge() {
-	    double result = 0;
-	    switch (getMovie().getPriceCode()) {
-	        case Movie.REGULAR:
-	            result += 2;
-	            if (getDaysRented() > 2)
-	                result += (getDaysRented() - 2) * 1.5;
-	            break;
-	        case Movie.NEW_RELEASE:
-	            result += getDaysRented() * 3;
-	            break;
-	        case Movie.CHILDRENS:
-	            result += 1.5;
-	            if (getDaysRented() > 3)
-	                result += (getDaysRented() - 3) * 1.5;
-	            break;
-	    }
-	    return result;
-	}
+    double getCharge() {
+    	return _movie.getCharge(_daysRented);
+    }
+    
 	int getFrequentRenterPoints() {
-		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) 
-		   return 2;
-		else
-			return 1;
+		return _movie.getFrequentRenterPoints(_daysRented);
 	}
 }
