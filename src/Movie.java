1c8de7109138e86748e3cdcfcdcaa460fe2e7008
Dangochan <francescakoeller@gmail.com>
2015-04-21 12:07:28 +0200

    8. created price class

--- src/Movie.java
+++ src/Movie.java
@@ -1,21 +1,36 @@
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
 public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
-    private String title;
-    private int priceCode;
-    public Movie(String newtitle, int newpriceCode) {
-        title = newtitle;
-        priceCode = newpriceCode;
+    private String _name;
+    private int _priceCode;
+    private Price _price;
+    public Movie(String name, int priceCode) {
+        _name = name;
+        setPriceCode(priceCode);
     }
     public int getPriceCode() {
-        return priceCode;
+        return _price.getPriceCode();
     }
     public void setPriceCode(int arg) {
-        priceCode = arg;
+        switch (arg){
+        case REGULAR:
+        	_price = new RegularPrice();
+        	break;
+        case CHILDRENS:
+        	_price = new ChildrensPrice();
+        	break;
+        case NEW_RELEASE:
+        	_price = new NewReleasePrice();
+        	break;
+        	default:
+        		throw new IllegalArgumentException("Incorrect PriceCode");
+        }
     }
     public String getTitle (){
-        return title;
+        return _name;
     }
 	double getCharge(int daysRented) {
 	    double result = 0;
